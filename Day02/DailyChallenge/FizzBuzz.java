66e3bf6256dcb1f10624699f84c216cf9291c5ad
Ratnadeep <ratna726728@gmail.com>
2025-05-06 23:19:51 +0530

    adding daily challenge

---
+++ Day02/DailyChallenge/FizzBuzz.java
@@ -0,0 +1,36 @@
+package Day02.DailyChallenge;
+//Write FizzBuzz from scratch; generate prime numbers up to 100
+public class FizzBuzz {
+    public static void main(String[] args) {
+        System.out.println("FizzBuzz:");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+
+        // Prime Numbers up to 100
+        System.out.println("\nPrime numbers up to 100:");
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    // Helper method to check for primality
+    public static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
+
